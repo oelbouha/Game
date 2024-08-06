854b85fedea66ba8491b7782f29536143066f036
Outman Elbouhali <oelbouha@c1r1p1.1337.ma>
2024-08-06 23:58:31 +0100

    working on keys events  is done

--- game.js
+++ game.js
@@ -10,6 +10,10 @@ class game {
 		this.gameCanvas = new game_Canvas();
 		this.context = this.gameCanvas.getContext();
 		this.canvas = this.gameCanvas.getCanvas();
+
+		this.canvas.tabIndex = 0;
+		this.canvas.focus();
+
 		
 		this.playerOneHand = new CustomImage("./assets/hand.png");
 		this.playerTwoHand = new CustomImage("./assets/player-two-hand.png");
@@ -25,7 +29,12 @@ class game {
 		this.playerTwoAttackButton = new CustomImage("./assets/attack-blue.png");
 		
 		this.handleCanvasClick = this.handleCanvasClick.bind(this);
+
     	this.canvas.addEventListener("click", this.handleCanvasClick);
+		this.canvas.addEventListener("keydown", this.handleKeyPress);
+
+		this.handleKeyPress = this.handleKeyPress.bind(this);
+
 
 		this.topButton = this.topRetreatButton;
 		this.bottomButton = this.bottomAttackButton;
@@ -37,7 +46,7 @@ class game {
 	}
 	
 	waitForImagesToLoad() {
-		let images = [this.playerOneHand, this.playerTwoHand, this.topButton, this.bottomButton];
+		let images = [this.playerTwoHand, this.topButton, this.bottomButton, this.playerOneHand];
 		let loadedImages = 0;
 		images.forEach((image) => {
 			if (image.loaded) {
@@ -56,16 +65,17 @@ class game {
 	
 	startGame() {
 		this.playerOne = new Player("top", "retreat", this.gameCanvas.getHeight(), this.playerTwoHand, this.playerOneHand);
-		this.playerTwo = new Player("buttom", "retreat", this.gameCanvas.getHeight(), this.playerOneHand, this.playerTwoHand);
+		this.playerTwo = new Player("buttom", "attack", this.gameCanvas.getHeight(), this.playerOneHand, this.playerTwoHand);
 
-		console.log(this.playerOne);
-		console.log(this.playerTwo);
+		// console.log(this.playerOne);
+		// console.log(this.playerTwo);
 		this.gameLoop();
 	}
 
 	gameLoop() {
 		this.playerOne.update();
 		this.playerTwo.update();
+	
 		this.drawAll();
 
 		requestAnimationFrame(() => this.gameLoop());
@@ -101,13 +111,20 @@ class game {
 	}
 
 	drawHands() {
-		let playerOneHandX = this.gameCanvas.getCenterX(this.playerOneHand.width);
-		let playerTwoHandY = this.playerTwo.getHandCurrentY();
-		this.playerOneHand.draw(this.context, playerOneHandX, playerTwoHandY);
-
 		let playerTwoHandX = this.gameCanvas.getCenterX(this.playerOneHand.width);
-		let playerOneHandY = this.playerOne.getHandCurrentY();
-		this.playerTwoHand.draw(this.context, playerTwoHandX, playerOneHandY);
+		let playerTwoHandY = this.playerOne.getHandCurrentY();
+		
+		let playerOneHandX = this.gameCanvas.getCenterX(this.playerOneHand.width);
+		let playerOneHandY = this.playerTwo.getHandCurrentY();
+		
+		if (this.playerOne.state === "attack") {
+			this.playerOneHand.draw(this.context, playerOneHandX, playerOneHandY);
+			this.playerTwoHand.draw(this.context, playerTwoHandX, playerOneHandY);
+		}
+		else {
+			this.playerTwoHand.draw(this.context, playerTwoHandX, playerTwoHandY);
+			this.playerOneHand.draw(this.context, playerOneHandX, playerOneHandY);
+		}
 	}
 
 	clearCanvas() {
@@ -133,6 +150,14 @@ class game {
 		this.bottomBackgroundColor = temp;
 	}
 
+	handleKeyPress(event) {
+		console.log(event);
+
+		if (event.key === "ArrowUp") {
+			console.log("Arrow Up pressed");
+		}
+	}
+
 	handleCanvasClick(event) {
 		let rect = this.canvas.getBoundingClientRect();
 		let x = event.pageX -  rect.left;
@@ -149,12 +174,12 @@ class game {
 				let state = this.playerTwo.state;
 				this.playerTwo.startAnimation(state);
             }
-			// if (this.playerTwo.isMissed) {
-			// 	console.log("Player 2 missed the attack     ...");
-			// 	this.gameManager.update(this.playerTwo);
-			// 	this.bottomButton = this.bottomRetreatButton;
-			// 	this.switchColors();
-			// }
+			if (this.playerTwo.isMissed) {
+				// console.log("Player 2 missed the attack     ...");
+				// this.gameManager.update(this.playerTwo);
+				// this.bottomButton = this.bottomRetreatButton;
+				// this.switchColors();
+			}
         }
     }
 	drawScore() {
