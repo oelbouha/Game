b6ebec5741d569473c242ec24d5d972a04a33831
Outman Elbouhali <oelbouha@c2r1p9.1337.ma>
2024-08-23 15:07:54 +0100

    working on the backend server : multiplayer now is working

--- back/handslap/consumers.py
+++ back/handslap/consumers.py
@@ -6,46 +6,80 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 # channels and groups
 
 
-connected_players = []
-index = 0
 
 class GameConsumer(AsyncWebsocketConsumer):
+    connected_players = []
+    index = 0
     async def connect(self):
-        connected_players.append(self)
+        GameConsumer.connected_players.append(self)
 
-        print ("Connected", len(connected_players))
-        # if len (connected_players) == 2:
-        #     print("two  Players  connected")
-        #     self.room_name = f'game_{index}'
-        #     self.room_group_name = f'game_{index}'
+        print ("Connected", len(GameConsumer.connected_players))
+        if len (GameConsumer.connected_players) == 2:
+            print("two  Players  connected")
+            self.room_name = f'game_{GameConsumer.index}'
+            self.room_group_name = f'game_{GameConsumer.index}'
 
-        #     # Add the player to the group 
-        #     await self.channel_layer.group_add(
-        #         self.room_group_name,
-        #         self.channel_name
-        #     )
-        # else:
-        #     await self.accept()
+            for player in GameConsumer.connected_players:
+                await self.channel_layer.group_add(
+                    self.room_group_name,
+                    player.channel_name
+                )
+                player.room_group_name = self.room_group_name
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'game_message',
+                    'message': 'Game Start'
+                }
+            )
+            
+            GameConsumer.connected_players = []
+            GameConsumer.index += 1
+
+        else:
+            print("Waiting for other player")
+            self.room_group_name = None
 
         await self.accept()
-
+        
         await self.send(text_data=json.dumps({
             'message': 'Connected'
         }))
-    index += 1
+
     async def disconnect(self, close_code):
         print("Disconnected")
-
+        if self in GameConsumer.connected_players:
+            GameConsumer.connected_players.remove(self)
+        if self.room_group_name:
+            await self.channel_layer.group_discard(
+                self.room_group_name,
+                self.channel_name
+            )
 
     async def receive(self, text_data):
-    
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        try:
+            text_data_json = json.loads(text_data)
+            message = text_data_json['message']
+            # print("Received: ", message)
+            # print("Sending: ", message)
+            if self.room_group_name:
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'game_message',
+                        'message': message
+                    }
+                )
+            else:
+                await self.send(text_data=json.dumps({
+                    'message': 'Waiting for other player'
+                }))
+        except:
+            print("Error")
+    async def game_message(self, event):
+        message = event['message']
 
-
-        # print("Received: ", message)
-        # print("Sending: ", message)
-
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
